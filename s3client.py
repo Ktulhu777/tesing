c10d6052f8a86ab6e874f46c226f6a2402c270e1
Ktulhu777 <asusdag40@gmail.com>
2024-06-15 14:51:58 +0300

    '3'

--- s3client.py
+++ s3client.py
@@ -2,7 +2,7 @@ from minio import Minio
 from config import host, access_key, secret_key
 
 
-s3client = Minio(host,
+s3client = Minio(endpoint=host,
                  access_key=access_key,
                  secret_key=secret_key,
                  secure=False)
